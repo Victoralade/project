cf3237f15b4d4a8490e98a93f9876f9d4449ce93
Victoralade <victoralade807@gmail.com>
2024-10-07 06:37:29 -0700

    project

---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+
+
+
+
+
+
+// const age = parseInt(prompt('enter your age'))
+
+// if (age >=23){
+//     alert("welcome to the strippers zone")
+// }else if (age < 23 && age >= 18){
+//     alert(`${age} is allowed into the bar, buh can't access the strippers zone yet `)
+// }else{
+//     alert("To young to get access")
+// }
+const age = parseInt(prompt('submit your age'))
+
+if (age >=12 && age <=19){
+    alert("welcome to boyscout camp")
+}else if (age < 19 && age >=20){
+    alert(`${age} si allowed to the squad, buh can't be one yet`)
+}else{
+    alert("Too young to belong")
+}if(age <=13 && age >10){
+    alert(`${age} new member`)
+}
+// }if (age >=20){
+    // alert("Too old join")
+// }
